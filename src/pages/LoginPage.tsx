fb89faa5fde5df8207a5defe39c4a69e95cef3fd
Anushka-Singh1 <anushka.sng2002@gmail.com>
2025-05-03 00:07:06 +0530

    Updated login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -159,36 +159,6 @@ export const LoginPage: React.FC = () => {
                 {isLogin ? 'Sign In' : 'Sign Up'}
               </Button>
             </form>
-            
-            <div className="mt-6">
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-300" />
-                </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">Or continue with</span>
-                </div>
-              </div>
-              
-              <div className="mt-6 grid grid-cols-2 gap-3">
-                <Button
-                  type="button"
-                  variant="outline"
-                  onClick={handleGoogleSignIn}
-                  disabled={loading}
-                >
-                  Google
-                </Button>
-                <Button
-                  type="button"
-                  variant="outline"
-                  onClick={handleFacebookSignIn}
-                  disabled={loading}
-                >
-                  Facebook
-                </Button>
-              </div>
-            </div>
           </CardContent>
           
           <CardFooter className="text-center">
